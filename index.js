510cac04124fc0884e8e456beba757878c63fe86
Antoine <141731510+antoinep23@users.noreply.github.com>
2024-03-01 17:37:15 +0000

    creation route accueil et template

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const http = require('http')
 const fs = require('fs')
 
 
+const tempAccueil = fs.readFileSync(`${__dirname}/templates/tempAccueil.html`, 'utf-8')
+
+
 const server = http.createServer((req, res) => {
-    res.end('Hello world')
+    const pathName = req.url
+    
+    if (pathName === '/' || pathName === '/accueil') {
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        res.end(tempAccueil)
+    }
+    else {
+        res.writeHead(404)
+        res.end('Page not found !')
+    }
 })
+
+
+
 
 server.listen(8000, () =>{
     console.log('Server launched on port 8000.✅')
